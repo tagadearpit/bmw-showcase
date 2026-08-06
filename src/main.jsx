e725b2484839ebf5c25276957aadfab85dcaaa7f
Arpit Tagade <143588157+tagadearpit@users.noreply.github.com>
2026-08-06 19:05:18 +0530

    Clean up main.jsx formatting and add comment

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Entry point for the Bavaria Concept showcase
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
